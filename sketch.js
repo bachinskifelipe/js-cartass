f6fdfb9b300835282ab9830a7fc7db3292d033ff
bachinskifelipe <bachinski.felipe@escola.pr.gov.br>
2024-07-24 16:50:37 -0300

    adicionando arquivos cartas js css

---
+++ sketch.js
@@ -0,0 +1,59 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }function draw() {
+      background("green");
+      
+      rect(0, 10, 100, 150);
+  }
+  function draw() {
+      background("geen");
+  
+      fill("red");
+      rect(0, 10, 100, 150);
+  }function draw() {
+      background("green");
+      
+      stroke("orange") 
+      fill("red") 
+      rect(0, 10, 100, 150)
+  }
+  function draw() {
+    background(220);
+    
+    stroke("orange");
+    fill("red");
+    rect(mouseX, mouseY, 100, 150);
+  }
+  function draw() {
+    background(220);
+    
+    stroke("orange");
+    fill("red");
+    
+    console.log(mouseIsPressed);
+    rect(mouseX, mouseY, 100, 150);
+  }
+  function setup() {
+    // cria uma tela de 400px de largura por 400px de altura
+    createCanvas(400, 400);
+  }
+  function setup() {
+    createCanvas(400, 400);
+    background("green")
+  }
+  
+  function draw() {
+    stroke("orange");
+    fill("red");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
+  
