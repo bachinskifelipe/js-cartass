8b54c1b6ee79e6d5608d80ab9551cf10afe928df
bachinskifelipe <bachinski.felipe@escola.pr.gov.br>
2024-08-14 16:42:39 -0300

    adicionei a licença de uso

--- sketch.js
+++ sketch.js
@@ -1,59 +1,18 @@
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(600, 600);
+   background("white");
+}
+
+
+function draw() {
   
-  function draw() {
-    background(220);
-  }function draw() {
-      background("green");
-      
-      rect(0, 10, 100, 150);
-  }
-  function draw() {
-      background("geen");
+  stroke ("blue");
+  fill ("red");
+ 
   
-      fill("red");
-      rect(0, 10, 100, 150);
-  }function draw() {
-      background("green");
-      
-      stroke("orange") 
-      fill("red") 
-      rect(0, 10, 100, 150)
-  }
-  function draw() {
-    background(220);
-    
-    stroke("orange");
-    fill("red");
-    rect(mouseX, mouseY, 100, 150);
-  }
-  function draw() {
-    background(220);
-    
-    stroke("orange");
-    fill("red");
-    
-    console.log(mouseIsPressed);
-    rect(mouseX, mouseY, 100, 150);
-  }
-  function setup() {
-    // cria uma tela de 400px de largura por 400px de altura
-    createCanvas(400, 400);
-  }
-  function setup() {
-    createCanvas(400, 400);
-    background("green")
-  }
+  // console.log(mouseIsPressed);
   
-  function draw() {
-    stroke("orange");
-    fill("red");
-    
-    
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 20, 35);
-    }
+  if (mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
   }
-  
-  
+}
